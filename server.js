4fa607ba6bf6a924c90475318860bf519a0c11eb
YairMoller <yairmoller@hilma.tech>
2024-11-19 17:03:37 +0200

    prime

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("node:http");
+const { json } = require("stream/consumers");
 
 http.createServer((req, res) => {
     const splittedURL = req.url.split("/");
@@ -33,6 +34,9 @@ http.createServer((req, res) => {
         case "/contacts":
             getFile(res, "./data/contacts.json");
             break;
+        case "/comps/prime/100":
+            prime(req, res);
+            break;
     }
 }).listen(8080);
 
@@ -58,4 +62,29 @@ const getContact = (res, num) => {
         res.write(JSON.stringify(contact));
         res.end();
     });
+};
+
+const factorial = (req, res) => {
+    const splittedURL = req.url.split("/");
+    let sum = 1;
+    for (let i = 1; i <= splittedURL[3]; i++) {
+        sum = sum * 1;
+    }
+
+    res.write(sum);
+    res.emd();
+};
+
+const prime = (req, res) => {
+    const splittedURL = req.url.split("/");
+    let primeArr = [];
+    for (let i = 1; i <= splittedURL[3]; i++) {
+        let flag = true;
+        for (let j = 2; j < i; j++) {
+            if ((i / j) % 1 === 0) flag = false;
+        }
+        if (flag) primeArr.push(i);
+    }
+    res.write(JSON.stringify(primeArr));
+    res.end();
 };
