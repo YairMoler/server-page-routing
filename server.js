e2db8c054373dcb549f6cb8be296fb415590479b
YairMoller <yairmoller@hilma.tech>
2024-11-19 11:15:12 +0200

    clean code

--- server.js
+++ server.js
@@ -2,34 +2,26 @@ const fs = require("fs");
 const http = require("node:http");
 
 http.createServer((req, res) => {
-    if (req.url === "/pages") {
-        fs.readFile("./html/pages.html", (err, data) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            res.write(data);
-            res.end();
-        });
-    }
-    if (req.url === "/pages/about") {
-        fs.readFile("./html/about.html", (err, data) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            res.write(data);
-            res.end();
-        });
-    }
-    if (req.url === "/pages/sports") {
-        fs.readFile("./html/sport.html", (err, data) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            res.write(data);
-            res.end();
-        });
+    switch (req.url) {
+        case "/pages":
+            getFile(res, "./html/pages.html");
+            break;
+        case "/pages/about":
+            getFile(res, "./html/about.html");
+            break;
+        case "/pages/sports":
+            getFile(res, "./html/sport.html");
+            break;
     }
 }).listen(8080);
+
+const getFile = (res, file) => {
+    fs.readFile(file, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        res.write(data);
+        res.end();
+    });
+};
